7d70b9f79802b8f33328929dc0a6b704091e47c6
danielvillajuan <danielvillajuan@hotmail.com>
2022-02-19 15:39:44 -0300

    añadiendo las rutas carrito y error

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Carrito from "../components/Carrito";
+import Error from "../components/Error";
 
 import Home from "../components/Home";
+import Navigation from "../components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact patch="/" component={Home} />
-      </Switch>
+      <Navigation>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/carrito" component={Carrito} />
+          <Route exact path="/error" component={Error} />
+        </Switch>
+      </Navigation>
     </BrowserRouter>
   );
 };
